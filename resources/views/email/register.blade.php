eb5f2675eb1e84bf57a4f183f7194d90af508bdb
mladen-boskovic <51060017+mladen-boskovic@users.noreply.github.com>
2019-05-27 21:33:43 +0200

    delete all except readme

--- resources/views/email/register.blade.php
+++
@@ -1,6 +0,0 @@
-<p>
- Zdravo <strong>{{$name}}</strong>!
-</p>
-<p>
-    Kliknite na link koji sledi kako bi aktivirali nalog: <a href="{{url('/')}}/activate/{{$token}}" target="_blank">Kliknite Ovde</a>
-</p>
